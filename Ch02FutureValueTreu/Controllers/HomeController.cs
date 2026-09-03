5f39570f199c38ddca8be5142198c38ef031c400
Georgia-Treu <treugeorgia@gmail.com>
2026-09-02 19:25:29 -0500

    pushing project to GitHub

---
+++ Ch02FutureValueTreu/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ch02FutureValueTreu.Models;
+
+namespace Ch02FutureValueTreu.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {            
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
